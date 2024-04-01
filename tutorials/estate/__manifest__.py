4e866ae4463bacb9f76c8325de8f329df7227b76
Gh4sst <alemaciasee64@gmail.com>
2024-04-01 11:58:32 +0200

    yes

---
+++ tutorials/estate/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'estate',
+    'version': '1.0',
+    'summary': 'Breve descripción del módulo',
+    'author': 'Tu nombre',
+    'license': 'AGPL-3',
+    'website': 'https://www.ejemplo.com',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
